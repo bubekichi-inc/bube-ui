9bbdd56470676013f348e783662d6e035ba2e1e5
shuheitachikawa <shuhei.tusx@gmail.com>
2024-08-22 07:27:41 +0900

    s

--- eslint.config.js
+++ eslint.config.js
@@ -1,91 +1,28 @@
-// import globals from "globals";
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
-// const config = [
-//   {
-//     files: ["**/*.{ts,tsx,js,jsx}"],
-//     languageOptions: {
-//       globals: {
-//         ...globals.browser,
-//         ...globals.node,
-//       },
-//       parserOptions: {
-//         ecmaVersion: "latest",
-//       },
-//     },
-//     rules: {
-//       camelcase: "warn",
-//       eqeqeq: "warn",
-//       "init-declarations": "warn",
-//       "no-nested-ternary": "warn",
-//       "no-await-in-loop": "warn",
-//       "no-cond-assign": "warn",
-//       "no-const-assign": "warn",
-//       "no-constant-condition": "warn",
-//       "no-dupe-keys": "warn",
-//       "no-else-return": "warn",
-//       "no-empty": "warn",
-//       "no-empty-function": "error",
-//       "no-ex-assign": "warn",
-//       "no-extra-boolean-cast": "warn",
-//       "no-fallthrough": "warn",
-//       "no-func-assign": "warn",
-//       "no-irregular-whitespace": "warn",
-//       "no-param-reassign": "warn",
-//       "no-parsing-error": "off",
-//       "no-return-await": "warn",
-//       "no-script-url": "warn",
-//       "no-self-assign": "warn",
-//       "no-self-compare": "warn",
-//       "no-undef": "warn",
-//       "no-unreachable": "warn",
-//       "no-unsafe-negation": "warn",
-//       "no-unused-expressions": "warn",
-//       "no-use-before-define": "warn",
-//       "no-useless-concat": "warn",
-//       "no-useless-return": "warn",
-//       "no-var": "warn",
-//       "@typescript-eslint/no-empty-function": "warn",
-//       yoda: "warn",
-//       "unused-imports/no-unused-imports": "warn",
-//       "import/order": [
-//         "error",
-//         {
-//           groups: [
-//             "builtin",
-//             "external",
-//             "parent",
-//             "sibling",
-//             "index",
-//             "object",
-//             "type",
-//           ],
-//           pathGroups: [
-//             {
-//               pattern: "@/**",
-//               group: "parent",
-//               position: "before",
-//             },
-//           ],
-//           alphabetize: {
-//             order: "asc",
-//           },
-//           "newlines-between": "always",
-//         },
-//       ],
-//       "import/no-duplicates": "error",
-//     },
-//   },
-// ];
-
-// export default config;
-
-export default [
+export default tseslint.config(
+  { ignores: ["dist"] },
   {
-    files: ["**/*.{ts,tsx,js,jsx}"],
-    rules: {
-      semi: "error",
-      "prefer-const": "error",
-      "no-empty-function": "error",
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
-  },
-];
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  }
+);
