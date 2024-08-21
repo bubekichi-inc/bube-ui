b8f373006073190b9716754773178edbd8d230ca
shuheitachikawa <shuhei.tusx@gmail.com>
2024-08-22 07:53:57 +0900

    s

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import unusedImports from "eslint-plugin-unused-imports";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -23,6 +24,39 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
+      camelcase: "warn",
+      eqeqeq: "warn",
+      "init-declarations": "warn",
+      "no-nested-ternary": "warn",
+      "no-await-in-loop": "warn",
+      "no-cond-assign": "warn",
+      "no-const-assign": "warn",
+      "no-constant-condition": "warn",
+      "no-dupe-keys": "warn",
+      "no-else-return": "warn",
+      "no-empty": "warn",
+      "no-empty-function": "warn",
+      "no-ex-assign": "warn",
+      "no-extra-boolean-cast": "warn",
+      "no-fallthrough": "warn",
+      "no-func-assign": "warn",
+      "no-irregular-whitespace": "warn",
+      "no-param-reassign": "warn",
+      "no-parsing-error": "off",
+      "no-return-await": "warn",
+      "no-script-url": "warn",
+      "no-self-assign": "warn",
+      "no-self-compare": "warn",
+      "no-undef": "warn",
+      "no-unreachable": "warn",
+      "no-unsafe-negation": "warn",
+      "no-unused-expressions": "warn",
+      "no-use-before-define": "warn",
+      "no-useless-concat": "warn",
+      "no-useless-return": "warn",
+      "no-var": "warn",
+      "@typescript-eslint/no-empty-function": "warn",
+      yoda: "warn",
     },
   }
 );
--- packages/bube-ui-react/src/stories/Button.tsx
+++ packages/bube-ui-react/src/stories/Button.tsx
@@ -27,7 +27,7 @@ export interface ButtonProps {
 /**
  * Primary UI component for user interaction
  */
-export const Button: React.FC = ({
+export const Button = ({
   primary = false,
   size = "medium",
   backgroundColor,
