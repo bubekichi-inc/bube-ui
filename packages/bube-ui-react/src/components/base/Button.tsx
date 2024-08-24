621f51bf19b54ae5ae969b29117918135980c8ab
shuheitachikawa <shuhei.tusx@gmail.com>
2024-08-24 12:37:25 +0900

    s

--- packages/bube-ui-react/src/components/base/Button.tsx
+++ packages/bube-ui-react/src/components/base/Button.tsx
@@ -1,15 +1,41 @@
-import React from "react";
-export interface Props {
-  children: React.ReactNode;
-}
+import type { ComponentProps, ForwardedRef, ReactNode } from "react";
+import { forwardRef } from "react";
 
-/**
- * Primary UI component for user interaction
- */
-export const Button: React.FC<Props> = ({ children }) => {
-  return (
-    <button type="button" className="bg-red-500 text-blue-500 text-2xl">
-      {children}
-    </button>
-  );
-};
+type Props = {
+  // variant?: "primary" | "sub" | "danger" | "sub-danger" | "gray";
+  // disabled?: boolean;
+  // rounded?: boolean;
+  // expand?: boolean;
+  // size?: "xs" | "sm" | "md" | "lg";
+  // icon?: TIcon;
+  // iconPosition?: "left" | "right";
+  children?: ReactNode;
+} & ComponentProps<"button">;
+
+export const Button = forwardRef(
+  (
+    { disabled = false, children, ...props }: Props,
+    ref: ForwardedRef<HTMLButtonElement>
+  ) => {
+    return (
+      <button ref={ref} {...props} disabled={disabled}>
+        <div
+          align="center"
+          justify="center"
+          gap="xs2"
+          reverse={iconPosition === "right"}
+          nowrap
+        >
+          {icon && (
+            <WizIcon
+              icon={icon}
+              color={variantColor[variant]}
+              size={iconSize[size]}
+            />
+          )}
+          {children}
+        </div>
+      </button>
+    );
+  }
+);
